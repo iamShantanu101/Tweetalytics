10aada456a4f8eac067490f2de643a8b1b9a5c6c
Shantanu Deshpande <shantanud106@gmail.com>
2016-12-25 14:37:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='smm',
     version='0.2',
     packages=['smm'],
-    url='https://github.com/cyhex/streamcrab',
+    url='https://github.com/iamShantanu101/Tweetalytics',
     license='GPLv3',
-    author='Timor A.',
-    author_email='timor@cyhex.com',
+    author='Shantanu Deshpandde',
+    author_email='shantanud106@gmail.com',
     description='Real-Time, multi-lingual Twitter sentiment analyzer engine',
     install_requires=[
         "requests",
